ddbb7569da816644f77c3a2f8c4497f428dddf04
Mincong HUANG <mincong.h@gmail.com>
2021-09-11 18:02:13 +0200

    Fix tag

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 IMAGE_NAME='finance-toolkit'
-short_commit=$(git rev-parse --short HEAD)
 
+tag="$(git rev-parse --short HEAD)"
 script_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 docker_dir="${script_dir}/.."
 
 docker build \
   --tag "${IMAGE_NAME}:${tag}" \
+  --tag "latest" \
   "$docker_dir"
