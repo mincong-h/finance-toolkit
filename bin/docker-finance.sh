06b21a18bfcbe72a1e1d673216f9815d45a5bff1
Mincong HUANG <mincong.h@gmail.com>
2022-03-20 17:41:50 +0100

    Change start script

--- bin/docker-finance.sh
+++ bin/docker-finance.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+REGISTRY_NAME="registry-intl.cn-hongkong.aliyuncs.com"
+IMAGE_NAMESPACE="jimidata-prod"
+
 docker run \
     --rm \
     --volume "${HOME}/Downloads:/data/source" \
     --volume "${HOME}/gitty/finance-data:/data/target" \
-    finance-toolkit $@
+    "${REGISTRY_NAME}/${IMAGE_NAMESPACE}/finance-toolkit:latest" $@
