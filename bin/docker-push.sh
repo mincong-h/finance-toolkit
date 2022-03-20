b8756e3a0094e704e4323f7c50aa82377485a7a2
Mincong Huang <mincong.h@gmail.com>
2022-03-20 17:26:50 +0100

    Replace Circle CI by GitHub actions (#44)
    * Remove Circle CI
    
    * Add GitHub actions
    
    * Install dependencies
    
    * Update build script
    
    * Add docker push
    
    * Build and push

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -5,16 +5,10 @@ REGISTRY_NAME="registry-intl.cn-hongkong.aliyuncs.com"
 IMAGE_NAMESPACE="jimidata-prod"
 IMAGE_NAME="finance-toolkit"
 
-script_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-project_dir="$(realpath ${script_dir}/..)"
-
 short_commit="$(git rev-parse --short HEAD)"
 pipeline_id="${GITHUB_RUN_ID:-0}"
 tag="v${pipeline_id}-${short_commit}"
 
-docker build \
-  --tag "${IMAGE_NAME}:${tag}" \
-  --tag "${IMAGE_NAME}:latest" \
+docker push \
   --tag "${REGISTRY_NAME}/${IMAGE_NAMESPACE}/${IMAGE_NAME}:${tag}" \
-  --tag "${REGISTRY_NAME}/${IMAGE_NAMESPACE}/${IMAGE_NAME}:latest" \
-  "$project_dir"
+  --tag "${REGISTRY_NAME}/${IMAGE_NAMESPACE}/${IMAGE_NAME}:latest"
