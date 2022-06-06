cd4b051ad5993241c8c6c357739ff114da0a21b9
Mincong HUANG <mincong.h@gmail.com>
2022-06-06 10:28:59 +0200

    Start using logging (__main__.py)

--- finance_toolkit/__main__.py
+++ finance_toolkit/__main__.py
@@ -22,25 +22,35 @@ from docopt import docopt
 
 from .tx import Configurator, merge, move
 
+import logging
+
 
 def main():
+    logging.basicConfig(level=logging.DEBUG)
+
     args = docopt(__doc__)
+    logging.debug(f"args={args}")
 
     home = Path.home()
+    logging.debug(f"home={home}")
 
     # Handle the finance folder
     finance_root = args["--finance-root"]
     if not finance_root:
-        # Check the envar
         env = os.getenv("FINANCE_ROOT")
+        logging.debug(
+            f"User did not provide argument '--finance-root', check environment variable: FINANCE_ROOT={env}"  # noqa
+        )
         if env:
             root = Path(env).expanduser()
         else:
             # Use the $HOME/finances folder by default
             root = home / "finances"
     else:
-        # Use the provided folder
+        logging.debug(f"User provided argument '--finance-root'")
         root = Path(finance_root).expanduser()
+
+    logging.debug(f"finance-root={finance_root}")
 
     cfg_path = root / "finance-tools.yml"
     cfg = Configurator.load(cfg_path)
