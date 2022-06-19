48458ebedb01409b531aa44b1f810d95a8f00584
jingwen-z <jingwen7.zheng@gmail.com>
2022-06-19 09:44:53 +0200

    pull docker image before running

--- bin/docker-finance.sh
+++ bin/docker-finance.sh
@@ -2,9 +2,11 @@
 
 REGISTRY_NAME="registry-intl.cn-hongkong.aliyuncs.com"
 IMAGE_NAMESPACE="jimidata-prod"
+image="${REGISTRY_NAME}/${IMAGE_NAMESPACE}/finance-toolkit:latest"
 
+docker pull "$image"
 docker run \
     --rm \
     --volume "${HOME}/Downloads:/data/source" \
     --volume "${HOME}/gitty/finance-data:/data/target" \
-    "${REGISTRY_NAME}/${IMAGE_NAMESPACE}/finance-toolkit:latest" $@
+    "$image" $@
