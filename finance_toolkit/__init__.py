b577b4adaa02927f02361ebde13d7fee5c0cea2f
Mincong Huang <mincong.h@gmail.com>
2024-01-08 10:03:33 +0100

    Update tool info in init

--- finance_toolkit/__init__.py
+++ finance_toolkit/__init__.py
@@ -1,9 +1,9 @@
 """Finance Tools"""
 
-__version__ = "0.1.0"
+__version__ = "1.3.0"
 __author__ = "The Finance Toolkit Team"
 __copyright__ = """
-Copyright (c) 2019-2020, The Finance Toolkit Team
+Copyright (c) 2019-2024, The Finance Toolkit Team
 
 Permission to use, copy, modify, and distribute this software and its
 documentation for any purpose and without fee or royalty is hereby
