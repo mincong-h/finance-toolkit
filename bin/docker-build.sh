559241c738f0c112703d921be3b28f46e114d82f
Mincong Huang <mincong.h@gmail.com>
2025-02-23 22:01:40 +0100

    Move Docker registry from Aliyun to DockerHub (#108)

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-REGISTRY_NAME="registry-intl.cn-hongkong.aliyuncs.com"
-IMAGE_NAMESPACE="jimidata-prod"
-IMAGE_NAME="finance-toolkit"
-
 script_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 project_dir="$(realpath ${script_dir}/..)"
 
@@ -13,8 +9,6 @@ pipeline_id="${GITHUB_RUN_ID:-0}"
 tag="v${pipeline_id}-${short_commit}"
 
 docker build \
-  --tag "${IMAGE_NAME}:${tag}" \
-  --tag "${IMAGE_NAME}:latest" \
-  --tag "${REGISTRY_NAME}/${IMAGE_NAMESPACE}/${IMAGE_NAME}:${tag}" \
-  --tag "${REGISTRY_NAME}/${IMAGE_NAMESPACE}/${IMAGE_NAME}:latest" \
+  --tag "mc144/finance-toolkit:${tag}" \
+  --tag "mc144/finance-toolkit:latest" \
   "$project_dir"
--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-REGISTRY_NAME="registry-intl.cn-hongkong.aliyuncs.com"
-IMAGE_NAMESPACE="jimidata-prod"
-IMAGE_NAME="finance-toolkit"
-
 short_commit="$(git rev-parse --short HEAD)"
 pipeline_id="${GITHUB_RUN_ID:-0}"
 tag="v${pipeline_id}-${short_commit}"
 
-docker push "${REGISTRY_NAME}/${IMAGE_NAMESPACE}/${IMAGE_NAME}:${tag}"
-docker push "${REGISTRY_NAME}/${IMAGE_NAMESPACE}/${IMAGE_NAME}:latest"
+docker push "mc144/finance-toolkit:${tag}"
+docker push "mc144/finance-toolkit:latest"
