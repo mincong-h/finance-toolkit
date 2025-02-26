2414e61e94e6cfb88872f474b588cbc8185999a1
Mincong Huang <mincong.h@gmail.com>
2025-02-26 23:20:40 +0100

    update

--- bin/docker-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-short_commit="$(git rev-parse --short HEAD)"
-pipeline_id="${GITHUB_RUN_ID:-0}"
-tag="v${pipeline_id}-${short_commit}"
-
-docker push "mc144/finance-toolkit:${tag}"
-docker push "mc144/finance-toolkit:latest"
